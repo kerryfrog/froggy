ffc8e0ab24cf1093beaa920b852eeaa1380b9bfa
Lee Dasom <70635175+kerryfrog@users.noreply.github.com>
2022-06-15 14:28:49 +0900

    누더기로변경됨

--- server/public/ravelryApi.js
+++ server/public/ravelryApi.js
@@ -1,7 +1,6 @@
 const BASE_URI = "https://api.ravelry.com";
 const endpoint = `${BASE_URI}/patterns/1234.json`;
-let imageUrls = [];
-
+let patternidList = [];
 //인증을 위한 헤더
 const headers = new Headers();
 headers.append(
@@ -13,29 +12,30 @@ headers.append(
         "nKTdyxY5v3mF3TDb8hkQHGhHwHNQL7lbJtU8Dz15"
     )
 );
-//headers.append("Content-Type", "text/xml");
-//headers.append("Origin", "https://api.ravelry.com");
-//headers.append("Access-Control-Allow-Methods", "GET");
+//이미지 uri 불러와서 리턴하는 함수
+async function getImageUrl() {
+  let imageUrls = [];
+  for (let i = 1234; i < 10; i++) {
+    url = `https://api.ravelry.com/patterns/${i + 1000}.json`;
+    await fetch(url, { method: "GET", headers: headers })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        tmpurl = data.pattern.photos[0].square_url;
+        imageUrls.push(tmpurl);
+      });
+  }
+  return imageUrls;
+}
 
 async function button_click() {
-  const url = `${BASE_URI}/patterns/`;
+  let url;
+  let data;
   console.log("button clicked!");
-  //console.log(endpoint);
-  fetch(endpoint, { method: "GET", headers: headers })
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-      imageUrls.push(data.pattern.photos.square_url);
-      console.log(data.pattern.photos.0.square_url);
-    });
-  //data = res.json();
-  /*
-  for (let i = 0; i < 10; i++) {
-    fetch(endpoint, { method: "GET", headers: headers })
-      .then((res) => res.json())
-      .then((res) => console.log(res));
-    const data = await res.json();
+  for (let i = 1234; i < 10; i++) {
+    url = `${BASE_URI}/patterns/${i + 1000}.json`;
+    data = await getImageUrl(url);
     imageUrls.push(data);
   }
-  */
+  console.log(imageUrls);
 }
